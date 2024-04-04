c33f1291aeae9d13d452373fc99f56b91905afa1
Mohammed inane <inane.mohammed@gmail.com>
2024-04-05 00:46:37 +0200

    authorize front, js and css files

--- backend/src/main/java/edu/miage/springboot/Application.java
+++ backend/src/main/java/edu/miage/springboot/Application.java
@@ -2,6 +2,8 @@ package edu.miage.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 
 @SpringBootApplication
@@ -9,5 +11,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web.ignoring().requestMatchers("/", "/index.html", "*.ico", "*.css", "*.js");
     }
 }
