d4eae74662b6755cbe75474addfb1d54466ac3a9
SASdiop22 <serigneasdiop@gmail.com>
2026-01-14 15:33:17 +0100

    Gestion des recrutement début

--- backend/src/main/java/edu/miage/springboot/dao/repositories/users/UserRoleRepository.java
+++ backend/src/main/java/edu/miage/springboot/dao/repositories/users/UserRoleRepository.java
@@ -3,5 +3,8 @@ package edu.miage.springboot.dao.repositories.users;
 import edu.miage.springboot.dao.entities.users.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
+    Optional<UserRoleEntity> findByName(String roleName);
 }
---
+++ backend/src/main/java/edu/miage/springboot/services/impl/offers/MatchingService.java
@@ -0,0 +1,4 @@
+package edu.miage.springboot.services.impl.offers;
+
+public class MatchingService {
+}
